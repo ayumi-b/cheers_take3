bb6c0b4420b06ff9f94764afcb6e3248bef285c4
Ayumi Fukuda Bennett <ayumi.fukuda@gmail.com>
2015-05-05 10:20:07 -0500

    initial tests for start

---
+++ test/integration/test_cheers.rb
@@ -0,0 +1,10 @@
+require_relative 'helper'
+
+class TestCheersIntegration < Minitest::Unit::TestCase
+
+  def test_zero_arguments_prints_help_message
+    output = `./cheers`
+    expected = "I can't cheer for nothing!"
+    assert_equal expected, output
+  end
+end
